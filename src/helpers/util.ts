5b5cdbdbb2a901e3d7ce34a0f75971b3e912a6ef
LHMFighting <2585283373@qq.com>
2019-09-24 23:24:42 +0800

    上传和下载的进度监控

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -1,6 +1,6 @@
 const toString = Object.prototype.toString
 
-export function isDate (val: any): val is Date {
+export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
 
@@ -8,8 +8,12 @@ export function isDate (val: any): val is Date {
 //   return val !== null && typeof val === 'object'
 // }
 
-export function isPlainObject (val: any): val is Object {
+export function isPlainObject(val: any): val is Object {
   return toString.call(val) === '[object Object]'
+}
+
+export function isFormData(val: any): val is FormData {
+  return typeof val !== 'undefined' && val instanceof FormData
 }
 
 export function extend<T, U>(to: T, from: U): T & U {
